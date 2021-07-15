f667b9d1384d76be9763552673bb2c077805b9b1
edgardo90 <edgardo9000@gmail.com>
2021-07-15 19:57:39 -0300

    ejercicios JS-1 completo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -272,7 +272,7 @@ function esVocal(letra){
   if(letra==="a" || letra==="e" || letra==="i" || letra==="o" || letra==="u"){
     return "Es vocal";
   }else{
-    return "Dato incorrecto";
+    return "Dato incorrecto"
   }
 }
 
